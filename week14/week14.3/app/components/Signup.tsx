a5906fad3c4299fc955fdc942e8330a06aa9951b
KoyalkarAditya <koyalkaraditya123@gmail.com>
2024-03-31 18:35:47 +0530

    week 15.1

--- week14/week14.3/app/components/Signup.tsx
+++ week14/week14.3/app/components/Signup.tsx
@@ -35,8 +35,8 @@ export function Signup() {
                 placeholder="123456"
               />
               <button
-                onClick={() => {
-                  solve(username, password);
+                onClick={async () => {
+                  const res = await solve(username, password);
                 }}
                 type="button"
                 className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
